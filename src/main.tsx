f6cbaf1ccea746cca23b638fd292c1b9c4294ce6
lookprice <lookprice.me@gmail.com>
2026-04-01 11:21:46 +0300

    Firebase dosyalarini temizle

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { LanguageProvider } from './contexts/LanguageContext';
-import { FirebaseProvider } from './contexts/FirebaseContext';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
@@ -10,13 +9,11 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <FirebaseProvider>
-        <BrowserRouter>
+      <BrowserRouter>
           <LanguageProvider>
             <App />
           </LanguageProvider>
         </BrowserRouter>
-      </FirebaseProvider>
-    </ErrorBoundary>
+      </ErrorBoundary>
   </StrictMode>,
 );
